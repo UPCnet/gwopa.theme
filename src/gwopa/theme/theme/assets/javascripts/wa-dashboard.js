97cd6e1febf5ff2aa5e86734fd4db787d66a1005
alberto.duran <alberto.duran@upcnet.es>
2019-06-11 09:47:47 +0200

    Icon selected and icon basic for dashboard

--- src/gwopa/theme/theme/assets/javascripts/wa-dashboard.js
+++ src/gwopa/theme/theme/assets/javascripts/wa-dashboard.js
@@ -246,41 +246,6 @@ require([
         }
       }
     });
-    // $.ajax({
-    //   url: window.project_path + '/api-getCurrentStage',
-    //   method: 'GET',
-    //   data: params,
-    //   transport: function(params){
-    //     params.beforeSend = function(request){
-    //       request.setRequestHeader("Accept", "application/json");
-    //     };
-    //     return $.ajax(params);
-    //   },
-    //   success: function(resp) {
-    //     console.log(resp);
-    //     resp = JSON.parse(resp);
-    //     console.log(resp);
-    //     var specifics = resp[0];
-    //     for (var i=0; i < specifics.length; i++) {
-    //       $('#'+specifics[i].id).removeClass();
-    //       $('#'+specifics[i].id).addClass("item basic " + specifics[i].selected_monitoring);
-    //     }
-    //     var others = resp[1];
-    //     if (others.length > 0) {
-    //       $('#others').show();
-    //       $('.cuartafila').html('')
-    //       for (var i=0; i < others.length; i++) {
-    //         $('.cuartafila').append('<a id="' + others[i].id + '" class="item basic ' + others[i].selected_monitoring + '"><div class="item_image "><img alt="" title="" src="++theme++gwopa.theme/assets/images/w-others.png"></div><div class="item_text"><span>lalala</span></div></a>');
-    //         $('#'+others[i].id).removeClass();
-    //         $('#'+others[i].id).addClass("item basic " + others[i].selected_monitoring);
-    //         $('#'+others[i].id).children('.item_text').children().text(others[i].title_specific);
-    //       }
-    //     }
-    //     else {
-    //       $('#others').hide();
-    //     }
-    //   }
-    // });
   }
 
   function reDrawInfo() {
@@ -363,6 +328,12 @@ require([
         resp = JSON.parse(resp);
         var specifics = resp[0];
         for (var i=0; i < specifics.length; i++) {
+          if (specifics[i].selected_monitoring == 'notset') {
+            $('#'+specifics[i].id+' > div > img').attr("src", specifics[i].icon_basic);
+          }
+          else {
+            $('#'+specifics[i].id+' > div > img').attr("src", specifics[i].icon_url_selected);
+          }
           $('#'+specifics[i].id).removeClass();
           $('#'+specifics[i].id).addClass("item basic " + specifics[i].selected_monitoring);
         }
@@ -372,6 +343,12 @@ require([
           $('.cuartafila').html('')
           for (var i=0; i < others.length; i++) {
             $('.cuartafila').append('<a id="' + others[i].id + '" class="item basic ' + others[i].selected_monitoring + '"><div class="item_image "><img alt="" title="" src="++theme++gwopa.theme/assets/images/w-others.png"></div><div class="item_text"><span>lalala</span></div></a>');
+            if (others[i].selected_monitoring == 'notset') {
+              $('#'+others[i].id+' > div > img').attr("src", others[i].icon_basic);
+            }
+            else {
+              $('#'+others[i].id+' > div > img').attr("src", others[i].icon_url_selected);
+            }
             $('#'+others[i].id).removeClass();
             $('#'+others[i].id).addClass("item basic " + others[i].selected_monitoring);
             $('#'+others[i].id).children('.item_text').children().text(others[i].title_specific);
