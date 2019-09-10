6f1fba3574bfe51f31252ae6e0ddcaa7cd8936ec
pilar.marinas <pilar.marinas@upcnet.es>
2019-09-10 08:18:04 +0200

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='gwopa.theme',
-    version='1.0',
+    version='1.1.dev0',
     description="A theme add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
