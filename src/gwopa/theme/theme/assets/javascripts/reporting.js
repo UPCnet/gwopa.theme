fff920ca44694adda60ff5f044a1c3f1ed3600a8
pilar.marinas <pilar.marinas@upcnet.es>
2019-12-16 13:54:40 +0100

    Prova saveReport

--- src/gwopa/theme/theme/assets/javascripts/reporting.js
+++ src/gwopa/theme/theme/assets/javascripts/reporting.js
@@ -7,4 +7,23 @@ require([
         $('#modelProyectYears').modal('show');
     })
 
+/*    // Save Report
+    $('#saveReport').click(function(e) {
+        var params = {};
+        var url = window.location.href;
+        var project_path = url.substring(0, url.lastIndexOf("/reportPreview"));
+        var project_path_report = project_path + '/reporting'
+        debugger
+        var file = $(downloadReport).first().trigger( "click" );
+        var namefile = $("#filename").text() + "_" + $("#generation_report_date").text() + ".xlsx";
+
+        $.ajax({
+          url: project_path_report + '/createElement',
+          method: 'POST',
+          data: params,
+          success: function(resp)
+            { if(resp) {location.reload();}}
+        });
+    });*/
+
 });
