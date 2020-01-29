fed3e5bd672a8dad40ef7c46d29d89d7e05e9463
Iago López Fernández <iago.lopez@upcnet.es>
2020-01-29 11:11:55 +0100

    Refresh report

--- src/gwopa/theme/theme/assets/javascripts/report.js
+++ src/gwopa/theme/theme/assets/javascripts/report.js
@@ -7,4 +7,25 @@ require([
         var tab = url.split('#')[1];
         $('#reportPreviewProject .nav-item a[href="#' + tab + '"]').trigger('click');
     }
+
+    $('#refreshReport').on('click', function(){
+        if (confirm("Are you sure want to update the data in this report with what is currently on the platform?")) {
+            var params = {};
+            params.refresh = true;
+
+            var url = window.location.href
+            if (url.match('#')) {
+                url = url.split('#')[0]
+            }
+
+            $.ajax({
+                url: url,
+                method: 'POST',
+                data: params,
+                success: function(resp) {
+                    location.reload();
+                }
+            });
+        }
+    });
 });
